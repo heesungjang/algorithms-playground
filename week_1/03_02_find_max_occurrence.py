7c532a3d7d9fb73262563d7161fc970fe7c1f610
Heesung jang <heesungj7@gmail.com>
2021-06-12 11:52:55 +0900

    added question

--- week_1/03_02_find_max_occurrence.py
+++ week_1/03_02_find_max_occurrence.py
@@ -1,10 +1,26 @@
 input = "hello my name is sparta"
 
+alphabet_occurrence_array = [0] * 26
+
 
 def find_max_occurred_alphabet(string):
+    for char in string:
+        if not char.isalpha():
+            continue
+        alpha_index = ord(char) - ord("a")
+        alphabet_occurrence_array[alpha_index] += 1
 
-    pass
+    max_occurrence = 0
+    max_alphabet_index = 0
+    for index in range(len(alphabet_occurrence_array)):
+        alphabet_occurrence = alphabet_occurrence_array[index]
+        if alphabet_occurrence > max_occurrence:
+            max_alphabet_index = index
+            max_occurrence += alphabet_occurrence
 
-    find_max_occurred_alphabet(input)
+    return chr(max_alphabet_index + ord("a"))
 
-    print(result)
+
+result = find_max_occurred_alphabet(input)
+
+print(result)
