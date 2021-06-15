0b0f9aa339e91dc9d4c8871c47039687d1bf5d26
Heesung jang <heesungj7@gmail.com>
2021-06-15 21:29:39 +0900

    binary searching for int value

---
+++ week2/06_is_exisiting_target_number_binary.py
@@ -0,0 +1,26 @@
+finding_target = 15
+finding_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+
+def is_existing_target_number_binary(target, array):
+    current_min = 0
+    current_max = len(array) - 1
+    current_guess = (current_min + current_max) // 2
+
+    while current_min < current_max:
+        if array[current_guess] == target:
+            return True
+        elif array[current_guess] < target:
+            current_min = current_guess + 1
+        else:
+            current_max = current_guess - 1
+        current_guess = (current_max + current_guess) // 2
+
+    if array[current_guess+1] == target:
+        return True
+    else:
+        return False
+
+
+result = is_existing_target_number_binary(finding_target, finding_numbers)
+print(result)
