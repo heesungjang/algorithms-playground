f1d2837c09f7a812a341123f41f6e7e1b909730e
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-10-31 14:06:37 +0900

    recursion_2

--- javascript/Recursion_02.js
+++ javascript/Recursion_02.js
@@ -9,12 +9,20 @@ function f(n) {
 }
 
 // 재귀 함수 곱
+
+//      순번         x(n)       n       return              최종
+//      1             x(5)       5      5 * x(5-1)         5* 24 == 120
+//      2            x(4)       4      4 * x(4-1)         4* 6 == 24
+//      3            x(3)       3      3 * x(3-1)         3* 2 == 6
+//      4           x(2)        2      2 * x(2-1)         2* 1 == 2
+//      5           x(1)        1       1                       1
+
 function x(n) {
     if (n <= 1) {
-        return x;
+        return 1;
     }
 
-    return n * f(n - 1);
+    return n * x(n - 1);
 }
 console.log("재귀함수:", f(100));
-console.log("재귀함수:", x(100));
+console.log("재귀함수:", x(5));
