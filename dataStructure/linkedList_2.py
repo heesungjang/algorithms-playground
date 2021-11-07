ef49e13b87ec372d51776994254a480c301de17d
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-11-07 15:22:49 +0900

    .

--- dataStructure/linkedList_2.py
+++ dataStructure/linkedList_2.py
@@ -11,23 +11,56 @@ class Node:
 
 # 링크드 리스트(클래스)
 
-    class LinkedList:
-        def __init__(self,value):
-            """
-            1. 첫 head로 초기
-            2. 값으로 전달 받은 Node 설정
-            """
-            self.head = Node(value)  
+class LinkedList:
+    def __init__(self,value):
+        """
+        1. 첫 head로 초기
+        2. 값으로 전달 받은 Node 설정
+        """
+        self.head = Node(value)  
 
-        def append(self, value):
-            """
-            1. 링크드 리스트 노드 추가
-            2. 가장 마지막 Node 다음에 추가해야 하므로
-            3. 현재 Node의 next pointer 값이 없을때 까지 선회
-            4. 마지막 node가 cur 값으로 들어왔을때 next 값을 추가하는 Node로 할당
+    def append(self, value):
+        """
+        1. 링크드 리스트 노드 추가
+        2. 가장 마지막 Node 다음에 추가해야 하므로
+        3. 현재 Node의 next pointer 값이 없을때 까지 선회
+        4. 마지막 node가 cur 값으로 들어왔을때 next 값을 추가하는 Node로 할당
 
-            """
-            cur = self.head
-            while cur.next is not None:
-                cur = cur.next
-            cur.next = Node(value)
+        """
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def print_all(self):
+        """
+        현재 연결 리스트에 모든 Node를 출력하는 method
+        
+        1. 노드의 next 즉 다음 node가 없는 마지막 tail 노드까지 순회하며 데이터를 출력한다.
+        """
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+    def get_node(self, index):
+        """
+        index로 특정 노드 반환
+        1. count 0 즉 0번째 index부터 인자 값으로 받은 index까지 순환하며 node를 옮겨간다.
+        2. 해당 index에 이르면 현재 node를 반환.
+        """
+        node = self.head
+        count = 0
+        while count < index:
+            node = node.next
+            count += 1
+        return node
+
+
+# head_node = Node(1)
+# print(head_node.data) # 첫번째 노드
+
+new_linkedList = LinkedList(1)
+
+print(new_linkedList.get_node(0).data)
+
