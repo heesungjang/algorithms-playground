37b69218f33a17b6d509c0806f358e57a45417d8
Heesung jang <68782077+heesungjang@users.noreply.github.com>
2021-11-09 23:23:49 +0900

    .

--- dataStructure/dict-hash.py
+++ dataStructure/dict-hash.py
@@ -8,6 +8,7 @@ class Dict:
     
     def get(self, key):
         index = hash(key)%len(self.items)
+         
         return self.items[index]
     
 
@@ -18,3 +19,6 @@ print(new_dict.items)
 new_dict.put("fast", "빠른")
 
 print(new_dict.get("fast"))
+
+
+
