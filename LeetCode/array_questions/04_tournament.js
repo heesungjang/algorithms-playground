758e220d85d42d1be85f41ce3ebcb2b6dd3d2e04
Heesung Jang <heesungj7@gmail.com>
2021-12-07 18:03:22 +0900

    phew

---
+++ LeetCode/array_questions/04_tournament.js
@@ -0,0 +1,49 @@
+/**
+ * all teams matches all teams
+ * only two team begin and end each match
+ * if win gets  5 points
+ * [home, away]
+ * [0 away win, 1 home win]
+ *
+ * output = Python
+ */
+
+const competitions = [
+  ["HTML", "C#"],
+  ["C#", "Python"],
+  ["Python", "HTML"],
+];
+
+const results = [0, 0, 1];
+
+function MyGetWinner(competitions, results) {
+  let resultIdex = 0;
+  let scoreBoard = {};
+  let currentWinningTeam = "";
+
+  for (const competition of competitions) {
+    const [home, away] = competition;
+
+    let isHomeWon = results[resultIdex];
+
+    const winner = isHomeWon ? home : away;
+
+    if (!(winner in scoreBoard)) {
+      scoreBoard[winner] = 1;
+    } else {
+      scoreBoard[winner]++;
+    }
+
+    if (!currentWinningTeam) currentWinningTeam = winner;
+
+    if (currentWinningTeam && scoreBoard[winner] > scoreBoard[currentWinningTeam]) {
+      currentWinningTeam = winner;
+    }
+
+    resultIdex++;
+  }
+
+  return currentWinningTeam;
+}
+
+console.log(MyGetWinner(competitions, results));
