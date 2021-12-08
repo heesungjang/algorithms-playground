f98e9be41b6cc99247cf62297c422f1442f79806
Heesung Jang <heesungj7@gmail.com>
2021-12-08 12:13:07 +0900

    day5 leet code

--- LeetCode/Array/Day_05_find_min_non_constructible.js
+++ LeetCode/Array/Day_05_find_min_non_constructible.js
@@ -7,7 +7,25 @@
 const coins = [5, 7, 1, 1, 2, 3, 22];
 
 function getMinNonConstructible(coins) {
-  return coins;
+  // 코인 정렬
+  coins.sort((a, b) => a - b);
+
+  let minValueCandidate = 1;
+
+  // [1,1,2,3,5,7,22]
+  // 1 >  1?  x = 2
+  // 1 >  2? x = 3
+  // 2 > 3? x = 5
+  // 3 > 5? x = 8
+  // 5 > 8? x = 13
+  // 7 > 13? x = 20
+  // 22 > 20? o = return 20
+  for (let i = 0; i < coins.length; i++) {
+    if (coins[i] > minValueCandidate) return minValueCandidate;
+    else minValueCandidate = coins[i] + minValueCandidate;
+  }
+
+  return minValueCandidate;
 }
 
 console.log(getMinNonConstructible(coins));
