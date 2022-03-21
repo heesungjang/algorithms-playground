e43623f6d0e70ef72e5d7eb3fa082bc001e5a922
heesungjang <heesungj7@gmail.com>
2022-03-22 01:04:08 +1000

    leecode 151문제, 오..리스트 컨프리헨션으로 작성하면 이렇게 깔끔하군

--- python/leetcode/leetcode-151-reverse-words-in-string.py
+++ python/leetcode/leetcode-151-reverse-words-in-string.py
@@ -6,24 +6,29 @@ input3 = "a good   example"
 
 
 def reverse_words(s: str) -> str:
-    stack = []
-    temp_string = ""
+    # stack = []
+    # temp_string = ""
+    #
+    # for char in s.strip():
+    #     if len(temp_string) == 0 and char == " ":
+    #         continue
+    #
+    #     if char != " ":
+    #         temp_string += char
+    #         continue
+    #     else:
+    #         stack.append(temp_string)
+    #         temp_string = ""
+    #         continue
+    # stack.append(temp_string)
+    # stack.reverse()
+    # print(stack)
+    # print(" ".join(stack))
+    words = s.strip().split(" ")
+    words.reverse()
+    words = [word for word in words if word != ""]
 
-    for char in s.strip():
-        if len(temp_string) == 0 and char == " ":
-            continue
-
-        if char != " ":
-            temp_string += char
-            continue
-        else:
-            stack.append(temp_string)
-            temp_string = ""
-            continue
-    stack.append(temp_string)
-    stack.reverse()
-    print(stack)
-    print(" ".join(stack))
+    return " ".join(words)
 
 
-reverse_words(input1)
+print(reverse_words(input3))
