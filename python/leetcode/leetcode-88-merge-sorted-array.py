2464509972a84622ae7f35252528a4dbcdca81a9
heesungjang <heesungj7@gmail.com>
2022-03-22 18:01:48 +1000

    leetcode 88

---
+++ python/leetcode/leetcode-88-merge-sorted-array.py
@@ -0,0 +1,26 @@
+input1 = [1, 2, 3, 0, 0, 0]
+input2 = [2, 5, 6]
+m1 = 3
+n1 = 3
+
+
+def merge(nums1, m, nums2, n):
+    last = m + n - 1
+
+    while m > 0 and n > 0:
+        
+        if nums1[m - 1] < nums2[n - 1]:
+            nums1[last] = nums2[n - 1]
+            n -= 1
+        else:
+            nums1[last] = nums1[m - 1]
+            m -= 1
+        last -= 1
+
+    while n > 0:
+        nums1[last] = nums2[n - 1]
+        n -= 1
+        last -= 1
+
+
+merge(input1, m1, input2, n1)
