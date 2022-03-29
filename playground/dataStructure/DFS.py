5a18621a24d558f5e44dce44d12e767e621d3ef6
heesungjang <heesungj7@gmail.com>
2022-03-30 00:56:14 +1000

    dfs

--- playground/dataStructure/DFS.py
+++ playground/dataStructure/DFS.py
@@ -1,47 +1,14 @@
-graph = {
-    1: [2, 3, 4],
-    2: [5],
-    3: [5],
-    4: [],
-    5: [6, 7],
-    6: [],
-    7: [3],
-}
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
 
+    def add_child(self, value):
+        self.children.append(Node(value))
 
-def dfs_recursive(node, visited):
-    # 방문 처리
-    visited.append(node)
+    def depth_first_search(self, array):
+        array.append(self.value)
+        for child in self.children:
+            child.depth_first_search(array)
 
-    for adj in graph[node]:
-        if adj not in visited:
-            dfs_recursive(adj, visited)
-    return visited
-
-
-print(dfs_recursive(1, []))
-
-
-def dfs_stack(start):
-    # store visited nodes
-    visited = []
-
-    # store nodes to be visited
-    stack = [start]
-
-    # while there are nodes to be visited continue search(visit)
-    while stack:
-        concurrently_visiting_node = stack.pop()
-        visited.append(concurrently_visiting_node)
-
-        # from currently visiting node
-        for adj in graph[concurrently_visiting_node]:
-            # if adj node have not been visited yet,
-            if adj not in visited:
-                # add to be visited list
-                stack.append(adj)
-                
-    return visited
-
-
-print(dfs_stack(1))
+        return array
--- playground/dataStructure/stack.py
+++ playground/dataStructure/stack.py
@@ -1,25 +1,56 @@
 class Node:
-    def __init__(self, value, next):
+    def __init__(self, value, next=None):
         self.value = value
         self.next = next
+    # [curr]
+    # [next]
+    # [next]
 
 
 class Stack:
-    def __init__(self):
-        self.top = None
+    def __init__(self, value):
+        self.top = Node(value)
+        self.size = 0
 
     def push(self, value):
-        if self.top is None:
-            self.top = Node(value, None)
-        else:
-            curr_top = self.top
-            self.top = Node(value, curr_top)
+        new_node = Node(value)
+
+        if self.top:
+            new_node.next = self.top
+        self.top = new_node
+        self.size += 1
 
     def pop(self):
+        curr = self.top
+
+        while curr.next is not None:
+            curr = curr.next
+
+    def get_top(self):
         if self.top is not None:
-            curr_top = self.top
-            self.top = curr_top.next
-            return curr_top.value
+            return self.top.value
 
     def is_empty(self):
-        return self.top is None
+        pass
+
+    def clear(self):
+        pass
+
+    def print_all(self):
+        curr = self.top
+        stack = []
+        while curr is not None:
+            stack.append(curr.value)
+            curr = curr.next
+
+        print(stack)
+
+
+a = Stack(1)
+print(a.get_top())
+a.push(2)
+a.push(3)
+a.push(4)
+a.push(5)
+
+a.print_all()
