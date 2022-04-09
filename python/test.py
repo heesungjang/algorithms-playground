9d38a34e64405c74c48db7734fcab6ed327deeb3
heesungjang <heesungj7@gmail.com>
2022-04-10 03:18:54 +1000

    leet 819

--- python/test.py
+++ python/test.py
@@ -1,17 +1,19 @@
+import collections
 from typing import List
 
-
-def reverse_string(s: List[int]) -> List[int]:
-    left = 0
-    right = len(s) - 1
-
-    while left < right:
-        s[left], s[right] = s[right], s[left]
-        left += 1
-        right -= 1
-
-    return s
+a = "Bob hit a ball, the hit BALL flew far after it was hit."
 
 
-result = reverse_string(["h", "e", "l", "l", "o"])
+def most_common_word(paragraph: str, banned: List[str]) -> str:
+    # replace := 문자열 치환후, 새로운 문자열 반환
+    for c in "!?',;.": paragraph = paragraph.replace(c, " ")
+
+    words = [word for word in paragraph.lower().split() if word not in banned]
+
+    counter = collections.Counter(words)
+
+    return counter.most_common(1)[0][0]
+
+
+result = most_common_word(a, ["hit"])
 print(result)
