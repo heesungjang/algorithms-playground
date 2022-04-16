4843322c97f53c6ee4ba772da7e8e1aa1e28fbaf
heesungjang <heesungj7@gmail.com>
2022-04-17 01:33:00 +1000

    leetcode 2

--- playground/common_strategies/quick_sort.py
+++ playground/common_strategies/quick_sort.py
@@ -2,20 +2,21 @@ from typing import List
 
 
 def quick_sort(nums: List[int], low: int, high: int):
-    def partition(low, high):
+    # 파티션 함수
+    def partition(low: int, high: int) -> int:
+        # pivot 설정 - 오른쪽 끝 마지막 인덱스 선택 (로무토 파티션 계획)
         pivot = nums[high]
         left = low
-
         for right in range(low, high):
             if nums[right] < pivot:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
+
         nums[left], nums[high] = nums[high], nums[left]
-        
+
         return left
 
     if low < high:
-        # 중앙 정렬된 pivot 위치를 반환 받음
         pivot = partition(low, high)
 
         quick_sort(nums, low, pivot - 1)
