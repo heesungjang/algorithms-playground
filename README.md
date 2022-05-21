c7654a9c799d7e18e68419cfb69f12e1b7df34af
heesungjang <heesungj7@gmail.com>
2022-05-21 19:43:43 +1000

    리드미 바로가기 추가

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 📌 Study-algorithms
 
-
-
-### Sliding Window Questions:
-solutions: /playground/sliding_windows
+### Sliding Window:
 
 1. [leetcode 3. longest substring without repeating characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
-
+   ([바로 가기](https://github.com/heesungjang/algorithms-playground/blob/main/playground/sliding_windows/3.longest_substring_without_repeating_characters.py))
 2. [leetcode 121. Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)
+   ([바로 가기](https://github.com/heesungjang/algorithms-playground/blob/main/playground/sliding_windows/121.best_time_to_buy_and_sell.py))
+
+### Common Strategies
+
+1. 최대 공약수
+   구하기 ([바로 가기](https://github.com/heesungjang/algorithms-playground/blob/main/playground/common_strategies/fing_gcd.py))
+
--- playground/sliding_windows/3.longest_substring_without_repeating_characters.py
+++ playground/sliding_windows/3.longest_substring_without_repeating_characters.py
@@ -1,28 +1,27 @@
 def longest_substring(s: str) -> int:
-    # 왜 딕셔너리를 사용했을까요?
-    # 브루트포스로 이 문제를 푼다고 생각하면 O(n^3)겠죠?
-    # 모든 인덱스 위치에서 2중 포문으로 탐색을 하니까 n^2에
-    # 현재 관찰중인 substring 내에서 중복된 문자가 있는지 또 반복문을 돌면서 확인하겠죠? == n
-    # n^2 * n == n^3
+    """
+    브루트포스로 이 문제를 푼다고 생각하면 O(n^3)
+    모든 인덱스 위치에서 2중 포문으로 탐색을 하니까 n^2
+    현재 substring 내에서 중복된 문자 확인 n
+    """
 
-    sub = {}  # 딕셔너리에 저장하면 상수 시간에 중복 문자 탐색이 가능 합니다.
+    sub = {}
     curr_start = 0
     curr_len = 0
     longest = 0
 
     for i, letter in enumerate(s):
-        if letter in sub and sub[letter] >= curr_start:
-            curr_start = sub[letter] + 1
-            curr_len = i - sub[letter]
-            sub[letter] = i
+        if letter in sub and sub[letter] >= curr_start:  # 윈도우 내에서 중복 발생
+            curr_start = sub[letter] + 1  # 문자 시작점 오른쪽으로 한 칸 이동
+            curr_len = i - sub[letter]  # 윈도우 내 문자열 길이 갱신
+            sub[letter] = i  # 방문 처리
 
         else:
-            sub[letter] = i
+            sub[letter] = i  # 방문 처리
             curr_len += 1
             longest = max(curr_len, longest)
 
     return longest
 
 
-# abcabcbb
 assert longest_substring("abcabcbb") == 3
