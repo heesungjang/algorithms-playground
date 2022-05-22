9422c50e25145c1cfb2e9f7d0f490674c6599882
heesungjang <heesungj7@gmail.com>
2022-05-23 01:44:12 +1000

    재귀로 숫자 분할하기 추가

--- playground/common_strategies/recursively_divide_digits.py
+++ playground/common_strategies/recursively_divide_digits.py
@@ -15,7 +15,7 @@ f(15)   = f(1)   + 5 이다.
 
 
 def recursively_add_digits(n: int) -> int:
-    # n이 10보다 작다면 즉, 마지막 1의 자릿수라면,
+    # n이 10보다 작다면 즉, 분할해서 최종적으로 한자리 수가 재귀 호출에 들어갔을때
     if n < 10:
         return n
 
@@ -27,7 +27,7 @@ assert recursively_add_digits(1527) == 15
 
 # 같은 방법을 응용하면 각 자리 숫자의 제곱 합을 출력할 수 있다.
 def recursively_add_squared_digits(n: int) -> int:
-    # n이 10보다 작다면, 즉, 마지막 1의 자릿수라면 제곱을 리턴
+    # n이 10보다 작다면 즉, 숫자를 분할해서 최종적으로 한자리 수가 재귀 호출에 들어갔을때
     if n < 10:
         return n * n
 
@@ -36,3 +36,24 @@ def recursively_add_squared_digits(n: int) -> int:
 
 # 654321 = (6*6) + (5*5) + (4*4) + (3*3) + (2*2) + (1*1) == 91
 assert recursively_add_squared_digits(654321) == 91
+
+
+# 조금 더 응용해서 이번에는 짝수를 제외한 합을 구해보자.
+
+def recursively_add_odd_digits(n: int) -> int:
+    # 마지막 한자리 수의 값이 짝수라면 0을 리턴
+    if n < 10:
+        if n % 10 == 0:
+            return 0
+        else:
+            return n
+
+    last_digit = n % 10
+
+    if last_digit % 2 == 0:  # 짝수라면
+        return recursively_add_odd_digits(n // 10)
+    else:
+        return recursively_add_odd_digits(n // 10) + last_digit
+
+
+assert recursively_add_odd_digits(1527) == 13
