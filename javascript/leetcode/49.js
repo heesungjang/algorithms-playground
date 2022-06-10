93d43f736ca10af5bb9140ee32cf36046f518990
heesungjang <heesungj7@gmail.com>
2022-06-11 01:35:10 +1000

    49 js

--- javascript/leetcode/49.js
+++ javascript/leetcode/49.js
@@ -1,9 +1,14 @@
 const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 
 var groupAnagrams = function (strs) {
+  // 1.
   const obj = {};
+
   for (let str of strs) {
+    // eat   tea
     const sorted_str = str.split("").sort().join("");
+    // aet  aet
+
     if (sorted_str in obj) {
       obj[sorted_str].push(str);
     } else {
