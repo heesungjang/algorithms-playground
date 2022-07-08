2b3a1ffcec0eb66f1716ae189dc6ba144875b909
heesungjang <heesungj7@gmail.com>
2022-07-09 04:28:28 +1000

    백준 효율적인 해킹 결국 pypy로...

--- python/baekjoon/[1325]-효율적인 해킹.py
+++ python/baekjoon/[1325]-효율적인 해킹.py
@@ -3,42 +3,47 @@ import sys
 
 input = sys.stdin.readline
 
-n, m = map(int, input().split())  # n = 컴퓨터 수, m = 신뢰 줄
+n, m = map(int, input().strip().split())
 
-tree = [[] for _ in range(n + 1)]
+graph = [[] for _ in range(n + 1)]
 
-for i in range(m):
-    a, b = map(int, input().split())  # e.g) 5, 4
-    tree[b].append(a)
+for _ in range(m):
+    a, b = map(int, input().strip().split())
+    graph[b].append(a)
 
 
 def bfs(visited, node):
-    visited[node] = True
-    ret = 1
+    ret = 0
+    visited[node] = 1
 
-    queue = collections.deque()
-    queue.append(node)
+    q = collections.deque()
+    q.append(node)
 
-    while queue:
-        x = queue.popleft()
+    while q:
+        node = q.popleft()
 
-        for adj in tree[x]:
+        for adj in graph[node]:
             if not visited[adj]:
-                visited[adj] = True
                 ret += 1
-                queue.append(adj)
+                visited[adj] = visited[node] + 1
+                q.append(adj)
     return ret
 
 
-_max = 0
-answer = []
-for j in range(len(tree)):
-    visited = [False] * (n + 1)
-    cnt = bfs(visited, j)
-    if cnt > _max:
-        _max = cnt
-        answer = [j]
-    elif cnt == _max:
-        answer.append(j)
+cnts = []
+max_cnt = -sys.maxsize
+for i in range(1, n + 1):
+    visited = [0] * (n + 1)
+    cnt = bfs(visited, i)
+    cnts.append(cnt)
+    max_cnt = max(max_cnt, cnt)
 
-print(answer)
+answer = []
+
+for i, c in enumerate(cnts):
+    if c == max_cnt:
+        answer.append(i + 1)
+answer.sort()
+
+for a in answer:
+    print(a, end=" ")
