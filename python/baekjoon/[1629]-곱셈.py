c2544dae34a9d3341b1e10c16cd3ea3d0200b65b
heesungjang <heesungj7@gmail.com>
2022-07-10 19:51:38 +1000

    백준 곱셈

--- python/baekjoon/[1629]-곱셈.py
+++ python/baekjoon/[1629]-곱셈.py
@@ -1,3 +1,19 @@
 a, b, c = map(int, input().split())
 
-print(a ** b % c)
+
+def recur(a, b):
+    if b == 1:
+        return a % c
+
+    _c = recur(a, b // 2)
+
+    _c = (_c * _c) % c
+
+    if b % 2 == 1:
+        _c = (_c * a) % c
+    return _c
+
+
+answer = recur(a, b)
+
+print(answer)
