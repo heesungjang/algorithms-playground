354d7dc1646cab1c516169e37f945b26e5a1a219
heesungjang <heesungj7@gmail.com>
2022-10-25 16:35:22 +1000

    리트코드 자바스크립트 투썸

---
+++ javascript/leetcode/1.TwoSum.js
@@ -0,0 +1,34 @@
+/**
+ * 브루트포스
+ */
+
+var twoSome = function (nums, target) {
+  let answer;
+
+  for (i = 0; i < nums.length - 1; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        answer = [i, j];
+      }
+    }
+  }
+
+  return answer;
+};
+
+/**
+ * 해쉬
+ */
+
+var twoSome = function (nums, target) {
+  const hash = new Map();
+
+  for (i = 0; i < nums.length; i++) {
+    const pair = target - nums[i];
+    if (hash.has(pair)) {
+      return [i, hash.get(pair)];
+    } else {
+      hash.set(nums[i], i);
+    }
+  }
+};
